3af71c6e59e7acc73c7174eddb7f51ec4a75035d
Neerajyadav9466 <neerajrao4496@gmail.com>
2025-07-02 14:38:16 +0530

    Team 2 - Neeraj
    Added the temp folder with the login page that has the footer code

---
+++ temp/script.js
@@ -0,0 +1,17 @@
+const openBtn = document.getElementById("openLoginBtn");
+const closeBtn = document.getElementById("closeBtn");
+const modal = document.getElementById("loginModal");
+
+openBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+});
